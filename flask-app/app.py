9f0a9b69ab4cdd90c3dcad10a40af16e0f139101
Jasna Blemberg <jblemberg@learningobjects.com>
2018-07-25 10:03:43 -0400

    Replace broken cat gif links with a variety of animal gifs from giphy

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,20 +3,20 @@ import random
 
 app = Flask(__name__)
 
-# list of cat images
+# list of animal images
 images = [
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26388-1381844103-11.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr01/15/9/anigif_enhanced-buzz-31540-1381844535-8.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26390-1381844163-18.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-1376-1381846217-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/9/anigif_enhanced-buzz-3391-1381844336-26.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-29111-1381845968-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/9/anigif_enhanced-buzz-3409-1381844582-13.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr02/15/9/anigif_enhanced-buzz-19667-1381844937-10.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26358-1381845043-13.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-18774-1381844645-6.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-25158-1381844793-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/10/anigif_enhanced-buzz-11980-1381846269-1.gif"
+    "https://media.giphy.com/media/nNxT5qXR02FOM/giphy.gif",
+    "https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif",
+    "https://media.giphy.com/media/l0MYNB04rBb51QNtC/giphy.gif",
+    "https://media.giphy.com/media/11s7Ke7jcNxCHS/giphy.gif",
+    "https://media.giphy.com/media/Sj3cnA7JYMENy/giphy.gif",
+    "https://media.giphy.com/media/HnYefmiEyBKo0/giphy.gif",
+    "https://media.giphy.com/media/qLopWKCbz6UoM/giphy.gif",
+    "https://media.giphy.com/media/uwANH4MWtmGHe/giphy.gif",
+    "https://media.giphy.com/media/1P2puPdOem9tC/giphy.gif",
+    "https://media.giphy.com/media/9pq81D1EJ5XvG/giphy.gif",
+    "https://media.giphy.com/media/4nGYmLsCoHNXa/giphy.gif",
+    "https://media.giphy.com/media/WEGvLnkGLMqc0/giphy.gif"
 ]
 
 @app.route('/')
